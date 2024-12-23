4eacc46636c61f5a26e879218297ccb71b2b4f43
Jodexx <0984134q@gmail.com>
2024-12-23 01:34:12 +0100

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   url: 'https://wiki.jodexindustries.xyz',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/JodexIndustriesWiki/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
